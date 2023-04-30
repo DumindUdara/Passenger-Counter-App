2805809399d82c7805270154e1ea4d3bef67c3ca
Dumindu Udara <98957798+DumindUdara@users.noreply.github.com>
2023-04-30 11:50:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-// 1. Grab the save-el paragrah and store it in a variable called saveEl
+// create counterE1 variable 
 let countEl = document.getElementById("count-el")
+// craete saveE1 variable 
+// 1. Grab the save-el paragrah and store it in a variable called saveEl
 let saveEl = document.getElementById("save-el")
+// create saveNm variable 
 let saveNm = document.getElementById("save-nm")
 
 let count = 0
 
+
+// create increment function
 function increment() {
     count += 1
     countEl.textContent = count
 }
 
+// create save function
 function save() {
     let saveStr = count + " - "
     saveNm.textContent += saveStr
@@ -17,6 +23,7 @@ function save() {
     count = 0
 }
 
+// create clear function
 function clearBtn() {
     countEl.textContent = 0
     count = 0
